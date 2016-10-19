d5abc63f395340817039e7e34324958288309485
Kenneth Leine Schulstad <kenneth@leine.cc>
2016-10-19 23:38:36 +0200

    feat: improve ticket validation

--- common/models/ticket.js
+++ common/models/ticket.js
@@ -1,12 +1,26 @@
 'use strict';
 
 var every = require('lodash/every');
+var Checkit = require('checkit');
 
 module.exports = function(Ticket) {
-  Ticket.validate('rows', ticketValidator, {message: 'Invalid lotto ticket'});
-//  Ticket.validatesAbsenceOf('registeredTimestamp');
+
+  Ticket.validate('ticket', ticketValidator, {message: 'Invalid lotto ticket'});
+  Ticket.validate('rows', rowsValidator, {message: 'Invalid lotto ticket'});
+
 
   function ticketValidator(err) {
+    var checkit = new Checkit({
+      email: ['required', 'email']
+    });
+
+    var [checkitErr, res] = checkit.validateSync(this);
+    if (checkitErr) {
+      err("invalidEmail");
+    }
+  };
+
+  function rowsValidator(err) {
     if (this.rows.length === 0 ) {
       err("lottoTicketMustHaveAtLeast1Row")
     }
@@ -17,13 +31,15 @@ module.exports = function(Ticket) {
     for (var i = 0; i < this.rows.length; i++) {
       var row = this.rows[i];
 
-      if (row.length !== 7) {
+      if (row.length !== 7 && row.length !== 0) {
         err("lottoRowMustHave7Numbers");
       }
 
       if (! every(row, n => (n<35 && n>0))) {
         err("lottoRowMustBeNumbersBetween1And34");
       }
+
+      // TODO: Implement numbers must be unique
     }
   };
 
